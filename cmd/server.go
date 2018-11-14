7423b4392bea55e03249295e69969358e2801389
Christoph Held <c-held@web.de>
2018-11-14 14:38:20 +0100

    fix error handling

--- cmd/server.go
+++ cmd/server.go
@@ -16,17 +16,22 @@ func handleDisk(w http.ResponseWriter, r *http.Request) {
     _, err := ioutil.ReadFile(filepath)
   	if err != nil {
   	       http.Error(w, err.Error(), http.StatusInternalServerError)
-  	} else {
-      fmt.Fprintf(w, "ok")
-    }
+           return
+  	}
+    fmt.Fprintf(w, "ok")
 }
 
 func handleRemote(w http.ResponseWriter, r *http.Request) {
     resp, err := http.Get(remoteurl)
+    if err != nil {
+           http.Error(w, err.Error(), http.StatusInternalServerError)
+           return
+    }
     defer resp.Body.Close()
     _, err = ioutil.ReadAll(resp.Body)
     if err != nil {
            http.Error(w, err.Error(), http.StatusInternalServerError)
+           return
     }
     fmt.Fprintf(w, "ok")
 }
